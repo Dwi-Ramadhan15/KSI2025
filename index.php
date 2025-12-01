e6afc03854f5d6a461e44341ab03023f45ad072f
Dwi-Ramadhan15 <og303261@gmail.com>
2025-12-01 15:53:52 +0700

    Add simple addition function and main program check

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-echo "Halo, ini Tugas Pipeline Jenkins Saya berhasil!";
+// Fungsi penjumlahan sederhana
+function tambah($a, $b) {
+    return $a + $b;
+}
+
+// Cek jika dijalankan langsung (bukan di-include)
+if (!debug_backtrace()) {
+    echo "Program Utama Berjalan.\n";
+    echo "Hasil 5 + 5 adalah: " . tambah(5, 5);
+}
 ?>
